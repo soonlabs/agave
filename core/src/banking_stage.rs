1ce4c63667ba20f8ac526e6b04a9c8dca5dfd12b
Yan Mingzhi <realraindust@gmail.com>
2024-12-23 19:12:13 +0800

    feat: make packet filter mod public

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -70,7 +70,7 @@ mod latest_unprocessed_votes;
 mod leader_slot_timing_metrics;
 mod multi_iterator_scanner;
 pub mod packet_deserializer;
-mod packet_filter;
+pub mod packet_filter;
 mod packet_receiver;
 mod read_write_account_set;
 #[allow(dead_code)]
