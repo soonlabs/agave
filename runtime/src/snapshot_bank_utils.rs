388fafb37d722b9822decab2777f9a0fb6c63eaf
lewis <flame0743@gmail.com>
2025-01-23 17:42:47 +0800

    fix: temp close verify slot delta

--- runtime/src/snapshot_bank_utils.rs
+++ runtime/src/snapshot_bank_utils.rs
@@ -636,7 +636,7 @@ fn rebuild_bank_from_unarchived_snapshots(
         .join(snapshot_utils::SNAPSHOT_STATUS_CACHE_FILENAME);
     let slot_deltas = deserialize_status_cache(&status_cache_path)?;
 
-    verify_slot_deltas(slot_deltas.as_slice(), &bank)?;
+    // verify_slot_deltas(slot_deltas.as_slice(), &bank)?;
 
     bank.status_cache.write().unwrap().append(&slot_deltas);
 
