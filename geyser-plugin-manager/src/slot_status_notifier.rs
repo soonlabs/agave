ec221cfd23dd1caab7502fe239829e52a8683e1d
Lone G <gyllone1993@gmail.com>
2025-02-18 09:36:03 +0800

    debugging

--- geyser-plugin-manager/src/slot_status_notifier.rs
+++ geyser-plugin-manager/src/slot_status_notifier.rs
@@ -50,6 +50,7 @@ impl SlotStatusNotifierImpl {
             return;
         }
 
+        warn!("notify slot status: slot = {}, parent = {:?}, status = {:?}", slot, parent, slot_status);
         for plugin in plugin_manager.plugins.iter() {
             let mut measure = Measure::start("geyser-plugin-update-slot");
             match plugin.update_slot_status(slot, parent, slot_status) {
