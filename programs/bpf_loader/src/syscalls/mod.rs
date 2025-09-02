7a3c48a543d18c14c2044d8622d742073ffbbe8a
Lone G <gyllone1993@gmail.com>
2025-09-02 10:01:45 +0800

    debugging

--- programs/bpf_loader/src/syscalls/mod.rs
+++ programs/bpf_loader/src/syscalls/mod.rs
@@ -563,14 +563,10 @@ fn translate_slice_inner<'a, T>(
         return Err(SyscallError::InvalidLength.into());
     }
 
-    #[cfg(target_os = "zkvm")]
-    {
-        risc0_zkvm::guest::env::log(&format!("vm addr: {:#x}", vm_addr));
-    }
     let host_addr = translate(memory_mapping, access_type, vm_addr, total_size)?;
     #[cfg(target_os = "zkvm")]
     {
-        risc0_zkvm::guest::env::log(&format!("host addr: {:#x}", host_addr));
+        risc0_zkvm::guest::env::log(&format!("host addr: {:#x}, total size: {:#x}", host_addr, total_size));
     }
 
     if check_aligned && !address_is_aligned::<T>(host_addr) {
